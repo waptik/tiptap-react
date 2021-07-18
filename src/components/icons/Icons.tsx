7d60974ed586d47525af3e655cc7eab8e2c87f46
Stephane Mensah <waptik@users.noreply.github.com>
2021-07-18 14:20:52 +0000

    define icons to be used

---
+++ src/components/icons/Icons.tsx
@@ -0,0 +1,61 @@
+export {
+  Hash,
+  Bold as IconFormatBold,
+  CodeBlock as IconFormatCodeBlock,
+  Code as IconFormatCodeInline,
+  Minus as IconFormatDivider,
+  Happy as IconFormatEmoji,
+  Heading as IconFormatHeading,
+  Image as IconFormatImage,
+  // EDITOR
+  Italic as IconFormatItalic,
+  Link as IconFormatLink,
+  Unlink as IconFormatLinkRemove,
+  ListUl as IconFormatListBulleted,
+  ListOl as IconFormatListNumbered,
+  Hide as IconFormatSpoiler,
+  Strikethrough as IconFormatStrikethrough,
+  Underline as IconFormatUnderline,
+  Pin as IconPin,
+  Redo as IconRedo,
+  Undo as IconUndo,
+  Pen as IconPen,
+  LinkExternal as IconExternalLink
+} from '@emotion-icons/boxicons-regular';
+export {
+  // EDITOR
+  QuoteRight as IconFormatQuote
+} from '@emotion-icons/fa-solid';
+
+// brands
+export {
+  Discord as IconDiscord,
+  TelegramPlane as IconTelegram,
+  Twitch as IconTwitch,
+  Twitter as IconTwitter,
+  Markdown as IconMarkdown
+} from '@emotion-icons/fa-brands';
+export {
+  SortAlphaDown as IconAll,
+  Blog as IconBlog,
+  // CATEGORIES
+  Gamepad as IconCategoryGaming,
+  File as IconFile,
+  FileArchive as IconFileArchive,
+  FileAudio as IconFileAudio,
+  FileCode as IconFileCode,
+  FileCsv as IconFileCsv,
+  FileExcel as IconFileExcel,
+  FileImage as IconFileImage,
+  FilePdf as IconFilePdf,
+  FilePowerpoint as IconFilePowerpoint,
+  FileAlt as IconFileText,
+  FileVideo as IconFileVideo,
+  FileWord as IconFileWord
+} from '@emotion-icons/fa-solid';
+export { Mail as IconMail } from '@emotion-icons/octicons';
+
+export {
+  WrapText as IconFormatWrapText,
+  FormatClear as IconFormatClear
+} from '@emotion-icons/material';
