3a737c9ff618400799a60972e26c86c0a3fad1de
Stephane Mensah <waptik@users.noreply.github.com>
2021-07-18 14:28:20 +0000

    update <CTA/> link

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,31 +1,25 @@
-import { Link as ChakraLink, Button } from '@chakra-ui/react'
+import { Link as ChakraLink, Button } from '@chakra-ui/react';
 
-import { Container } from './Container'
+import { Container } from './Container';
 
 export const CTA = () => (
   <Container
     flexDirection="row"
     position="fixed"
     bottom="0"
-    width="100%"
+    width="full"
     maxWidth="48rem"
     py={3}
   >
-    <ChakraLink isExternal href="https://chakra-ui.com" flexGrow={1} mx={2}>
-      <Button width="100%" variant="outline" colorScheme="green">
-        chakra-ui
-      </Button>
-    </ChakraLink>
-
     <ChakraLink
       isExternal
-      href="https://github.com/vercel/next.js/blob/canary/examples/with-chakra-ui-typescript"
+      href="https://github.com/waptik/tiptap-react"
       flexGrow={3}
       mx={2}
     >
-      <Button width="100%" variant="solid" colorScheme="green">
+      <Button width="100%" variant="solid" colorScheme="blue">
         View Repo
       </Button>
     </ChakraLink>
   </Container>
-)
+);
