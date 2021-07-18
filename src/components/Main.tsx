2575b243e2a87eb18e80570ebe2963e775fff23b
Stephane Mensah <waptik@users.noreply.github.com>
2021-07-18 14:27:29 +0000

    remove padding from <Main/>

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,12 @@
-import { Stack, StackProps } from '@chakra-ui/react'
+import { Stack, StackProps } from '@chakra-ui/react';
 
 export const Main = (props: StackProps) => (
   <Stack
-    spacing="1.5rem"
+    spacing={6}
     width="100%"
     maxWidth="48rem"
-    mt="-45vh"
     pt="8rem"
     px="1rem"
     {...props}
   />
-)
+);
