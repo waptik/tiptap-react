08fd43ffcbf7d05f88e5004767388eb8b1a4e556
Stephane Mensah <waptik@users.noreply.github.com>
2021-07-18 14:35:17 +0000

    Show editor in main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,90 @@
 import {
   Link as ChakraLink,
+  Box,
   Text,
   Code,
   List,
   ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+  ListItem
+} from '@chakra-ui/react';
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons';
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Hero } from '../components/Hero';
+import { Container } from '../components/Container';
+import { Main } from '../components/Main';
+import { DarkModeSwitch } from '../components/DarkModeSwitch';
+import { CTA } from '../components/CTA';
+import { Footer } from '../components/Footer';
+import TiptapExample from '../components/TiptapExample';
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>typescript</Code>.
-      </Text>
+const Index = () => {
+  const links = [
+    {
+      href: 'https://www.tiptap.dev',
+      label: 'Tiptap headless editor v2'
+    },
+    {
+      href: 'https://nextjs.org',
+      label: 'Next.js'
+    },
+    {
+      href: 'https://chakra-ui.com',
+      label: 'Chakra-ui'
+    }
+  ];
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
+  return (
+    <Container height="100vh">
+      <Hero />
+      <Main>
+        <Text>
+          An example of Tiptap v2 react with Chakra-ui <Code>Tiptap v2</Code> +{' '}
+          <Code>Next.js</Code> + <Code>Chakra-ui</Code>.
+        </Text>
+
+        <TiptapExample />
+
+        <List spacing={3} my={0}>
+          {links.map(({ href, label }) => (
+            <ListItem key={label}>
+              <ListIcon as={CheckCircleIcon} color="blue.500" />
+              <ChakraLink
+                isExternal
+                href={href}
+                flexGrow={1}
+                mr={2}
+                color="blue.500"
+                _hover={{
+                  textDecoration: 'none'
+                }}
+              >
+                {label} <LinkIcon />
+              </ChakraLink>
+            </ListItem>
+          ))}
+        </List>
+      </Main>
+
+      <DarkModeSwitch />
+      <Footer>
+        <Text>
+          {' '}
           <ChakraLink
+            href="https://twitter.com/_waptik"
             isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
+            color="blue.500"
+            _hover={{
+              textDecoration: 'none'
+            }}
           >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+            Stephane
+          </ChakraLink>{' '}
+          made this with ❤️
+        </Text>
+      </Footer>
+      <CTA />
+    </Container>
+  );
+};
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
-
-export default Index
+export default Index;
